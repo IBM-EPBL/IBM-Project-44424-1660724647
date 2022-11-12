42e84eed1778a900f1ce436bd8da84322d49fad0
Jeffrey-Joan <57098615+Jeffrey-Joan@users.noreply.github.com>
2022-11-12 18:52:20 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 3/drownDetect.py
@@ -0,0 +1,73 @@
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+import cv2
+import time
+import numpy as np
+
+webcam = cv2.VideoCapture(0)
+
+if not webcam.isOpened():
+    print("Could not open webcam")
+    exit()
+
+t0 = time.time() 
+centre0 = np.zeros(2)
+isDrowning = False
+
+while webcam.isOpened():
+
+    
+    status, frame = webcam.read()
+
+    if not status:
+        print("Could not read frame")
+        exit()
+
+    
+    bbox, label, conf = cv.detect_common_objects(frame)
+    
+
+    
+    if (len(bbox) > 0):
+        bbox0 = bbox[0]
+        centre = [0, 0]
+
+ 
+        centre = [(bbox0[0] + bbox0[2]) / 2, (bbox0[1] + bbox0[3]) / 2]
+
+        # make vertical and horizontal movement variable
+        hmov = abs(centre[0] - centre0[0])
+        vmov = abs(centre[1] - centre0[1])
+
+
+        x = time.time()
+
+        threshold = 10
+        if (hmov > threshold or vmov > threshold):
+            print(x - t0, 's')
+            t0 = time.time()
+            isDrowning = False
+
+        else:
+
+            print(x - t0, 's')
+            if ((time.time() - t0) > 10):
+                isDrowning = True
+
+   
+        print('bbox: ', bbox, 'centre:', centre, 'centre0:', centre0)
+        print('Is he drowning: ', isDrowning)
+
+        centre0 = centre
+
+    out = draw_bbox(frame, bbox, label, conf, isDrowning)
+
+
+    cv2.imshow("Real-time object detection", out)
+
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
